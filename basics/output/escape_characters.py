d91c856c01eebb71eca51f36093aedf60a6ba4bf
AdrianaNizamova <>
2022-02-28 21:20:23 +0000

    Adriana

---
+++ basics/output/escape_characters.py
@@ -0,0 +1,7 @@
+print("\n -Display new line")
+print("\t -Indent by 4 spaces")
+print("\\ - Display a backslash")
+print("\" - Displaying a double-quote")
+print("\' - Displaying a single-quote")
+print( "\n\t I am Beep!\t\n" )
+      
